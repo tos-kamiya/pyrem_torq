857e8851b53bc4a66eb8d5d0940f488d2f503eb5
T. Kamiya <devnull@localhost>
2010-02-22 15:59:38 +0900

    Changed vertion to 0.2.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = [
 
 setup(
     name = 'pytorqy',
-    version = '0.1',
+    version = '0.2',
     packages = packages,
     package_dir = dict((p, "src/" + p.replace(".", "/")) for p in packages)
 )
