27abc2b2d8ed6c6636dd9f23d735c9a05d763b9d
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2012-01-23 12:46:35 +0900

    Added readme and license documents.

---
+++ README.rst
@@ -0,0 +1,11 @@
+==================
+Pyrem Torq README
+==================
+
+Pyrem Torq is a library for relaxed parsing of programming languages.
+
+Currently, supported programming languages are: C++03 (not 11), Java, Python.
+
+The official site is: http://www.remics.org/pyrem_torq/ .
+
+The development repository is: https://github.com/tos-kamiya/pyrem_torq/ .
--- setup.py
+++ setup.py
@@ -13,6 +13,28 @@ setup(
     name = 'pyrem_torq',
     version = '0.5',
     packages = packages,
-    package_dir = dict((p, "src/" + p.replace(".", "/")) for p in packages)
+    package_dir = dict((p, "src/" + p.replace(".", "/")) for p in packages),
+    package_data={'pyrem_torq': 
+        [
+            './LICENSE',
+            './README.rst',
+        ],
+    },
+    
+    license="MIT license",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
 )
 
